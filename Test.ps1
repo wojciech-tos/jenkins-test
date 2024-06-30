1e56eb25e2baca4f1328d8d83fd139d301eb0e52
wojciech-tos <wojciech.tos.1@gmail.com>
2024-06-30 23:46:18 +0200

    Update Test.ps1

--- Test.ps1
+++ Test.ps1
@@ -1,4 +1,3 @@
-New-Item -ItemType File -Path "C:\example"
-$msg = "Time difference IS NOT greater than 900 seconds"
-$msg | Out-File C:\example
-Get-Content C:\example
+$msg = "File content"
+$msg | Out-File "C:\Users\krzys\Downloads\example.txt"
+Get-Content "C:\Users\krzys\Downloads\example.txt"
